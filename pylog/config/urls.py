fc865d492e48afe466a59f443db8e9266ef82ae5
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2023-12-12 21:43:02 +0900

    pylog 실습 완료

--- pylog/config/urls.py
+++ pylog/config/urls.py
@@ -19,12 +19,16 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from config.views import index, post_list
+from config.views import index
+from blog.views import post_list, post_detail, post_add
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('posts/', post_list),
+    path('posts/<int:post_id>/', post_detail),
+    path('posts/add/', post_add),
 ]
 
 urlpatterns += static(
