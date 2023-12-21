6155114ec39a24f5d176e07103893e1ffe9043ac
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2023-12-21 23:26:36 +0900

    pystagram 실습 진행 3

--- pystargram/posts/urls.py
+++ pystargram/posts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from posts.views import feeds
+from posts.views import feeds, comment_add, comment_delete, post_add
 
 urlpatterns = [
     path("feeds/", feeds),
+    path("comment_add/", comment_add),
+    path(f"comment_delete/<int:comment_id>/", comment_delete),
+    path("post_add/", post_add),
 ]
--- pystargram/posts/views.py
+++ pystargram/posts/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.views.decorators.http import require_POST
+
+from posts.models import Post, Comment, PostImage
+from posts.forms import CommentForm, PostForm
 
 
 # Create your views here.
@@ -11,4 +16,61 @@ def feeds(request):
     if is_authenticated is False:
         return redirect("/users/login/")
 
-    return render(request, "posts/feeds.html")
+    posts = Post.objects.all()
+    comment_form = CommentForm()
+    context = {
+        "posts": posts,
+        "comment_form": comment_form,
+    }
+
+    return render(request, "posts/feeds.html", context)
+
+
+@require_POST
+def comment_add(request):
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.user = request.user
+
+        comment.save()
+
+        print(comment.id)
+        print(comment.content)
+        print(comment.user)
+
+        return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
+
+
+@require_POST
+def comment_delete(request, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+    if comment.user == request.user:
+        comment.delete()
+        return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
+
+    return HttpResponseForbidden("이 댓글을 삭제할 권한이 없습니다.")
+
+
+def post_add(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+
+            for image_file in request.FILES.getlist("images"):
+                PostImage.objects.create(
+                    post=post,
+                    photo=image_file,
+                )
+
+            url = f"/posts/feeds/#post-{post.id}"
+            return HttpResponseRedirect(url)
+
+    else:
+        form = PostForm()
+
+    context = {"form": form}
+    return render(request, "posts/post_add.html", context)
